a7087c2f040e0a641813a97f992e0cc7c2be1b91
Adesh Gupta <adeshgupta101@gmail.com>
2024-09-03 22:32:17 +0530

    Added requrements, updated API

--- testapi.py
+++ testapi.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import Optional
 
 
 
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
+
+
+class VectorInputConfig(BaseModel):
+    pooling_strategy: str
+
+
+class VectorInput(BaseModel):
+    text: str
+    config: Optional[VectorInputConfig] = None
+
 
 # Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
@@ -24,41 +35,46 @@ model = model.to(device)
 
 print("model ready for inference")
 
-def generate_embeddings_single(sentence):
+def generate_embeddings_single(sentence, config: VectorInputConfig):
     sentences = [sentence]
-    sentence_embeddings = generate_embeddings_batch(sentences)
+    sentence_embeddings = generate_embeddings_batch(sentences, config)
     return sentence_embeddings
 
 
-def generate_embeddings_batch(sentences):
+def generate_embeddings_batch(sentences, config: VectorInputConfig):
     encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt').to(device)
     with torch.inference_mode():
         model_output = model(**encoded_input)
-    sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+    
+    if config.pooling_strategy == 'mean':
+        sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+    else:
+        raise ValueError(f"Pooling strategy {config.pooling_strategy} not supported.")
+    
     sentence_embeddings = F.normalize(sentence_embeddings, p=2, dim=1)
     return sentence_embeddings
 
 
 app = FastAPI()
 
-
 class SentenceInput(BaseModel):
     sentence: str
 
 @app.post("/vectorize/")
-async def vectorize_sentence(input_data: SentenceInput):
+@app.post("/vectorize")
+async def vectorize(input: VectorInput):
     try:
-        # Perform inference to get the sentence embedding
-        sentence = input_data.sentence
-        sentence_embeddings = generate_embeddings_single(sentence)
+        SentenceInput = input.text
+        config = input.config
+        sentence_embeddings = generate_embeddings_single(SentenceInput, config)
         
-        # # Convert the tensor to a list
-        vector_list = sentence_embeddings.cpu().tolist()
-        vector_list = vector_list[0]
-
-        return {"vector": vector_list}
+        vectorize_handler = sentence_embeddings.cpu().tolist()
+        vectorize_handler = vectorize_handler[0]
+        
+        return {"text":input.text,"vector": vectorize_handler, "dim": len(vectorize_handler)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+        
 
 if __name__ == "__main__":
     import uvicorn
